18bd37e17d3208e52f663a06f24733cf10e085e2
DeveloperRooney <han_0427@naver.com>
2021-04-20 13:19:20 +0900

    bubble sort

--- src/main/kotlin/practice/Bubble.kt
+++ src/main/kotlin/practice/Bubble.kt
@@ -1,11 +1,20 @@
 package practice
 
 fun main() {
-
+    
+    // 숫자가 담긴 MutableList 생성
+    print("숫자 입력(띄어쓰기): ")
     var arr = readLine()!!.split(" ").map{it.toInt()}.toMutableList()
 
+    // 오름차순 정렬
+    // 배열의 길이만큼 반복
     for (x in 0 until arr.size) {
-        for (y in 0 until arr.size - x - 1) {
+
+        // 인덱스 최대값이 배열의 길이보다 1 작으므로 -1, 반복 회수는 -x
+        for (y in 0 until arr.size - 1 - x) {
+
+            // 인덱스 y와 인덱스 y의 오른쪽 인덱스와 비교하고
+            // 큰 수가 뒤로 가게 해준다.
             if (arr[y] > arr[y+1]) {
                 var temp = arr[y+1]
                 arr[y+1] = arr[y]
@@ -14,8 +23,9 @@ fun main() {
         }
     }
 
-    for (index in arr.indices) {
-        println(arr[index])
+    // 출력
+    for (x in arr.indices-1) {
+        println(arr[x])
     }
 }
 
