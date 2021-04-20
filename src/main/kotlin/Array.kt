8157ec11c3ab5df014177e9fcd55ca47cb09d9ff
DeveloperRooney <han_0427@naver.com>
2021-04-20 09:21:44 +0900

    test

--- src/main/kotlin/Array.kt
+++ src/main/kotlin/Array.kt
@@ -1,2 +1,22 @@
+fun main() {
+
+    var arr = Array<Int?>(5) {null}
+
+
+    // indices를 통해 .. 혹은 until을 이용할 필요 없이 아이템의 최소 인덱스부터 최대 인덱스까지 출력
+    for (x in arr.indices) {
+        print("숫자 입력 : ")
+        var num = readLine()!!.trim().toInt()
+        arr[x] = num
+        println(arr[x])
+
+    }
+
+    for (x in arr.indices) {
+        println(arr[x])
+    }
+
+}
+
 class Array {
 }
