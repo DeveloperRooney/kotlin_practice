6063009307f25f26f2a2c5373683362dc823efe5
DeveloperRooney <han_0427@naver.com>
2021-04-21 13:56:56 +0900

    게시판 관리 프로그램

--- src/main/kotlin/program/Board.kt
+++ src/main/kotlin/program/Board.kt
@@ -16,6 +16,7 @@ fun main() {
         print("명령어 입력 : ")
         val cmd = readLine()!!
 
+        // 새로운 글 작성
         if (cmd.equals("board write")) {
             println("새로운 글을 작성합니다.")
 
@@ -39,13 +40,20 @@ fun main() {
             boardList.add(board)
 
             println("${idx}번 글이 작성되었습니다.\n")
+
+
+        // 프로그램 종료    
         }else if (cmd.equals("board exit")) {
             println("프로그램을 종료합니다.")
             break
+        
+        // 글 삭제
         }else if (cmd.equals("board delete")) {
+            
+            // 삭제할 글 번호 입력
             print("삭제할 글 번호 : ")
             var num = readLine()!!.trim().toInt()
-
+            
             var cnt = 0
             for (x in 0 until boardList.size) {
                 if (boardList[x].idx == num-1) {
@@ -55,21 +63,24 @@ fun main() {
                     cnt++
                 }
             }
+
+            // 카운터가 글 갯수와 같아지면 해당 글이 없음
             if (cnt == boardList.size) {
                 println("해당 글은 존재하지 않습니다.\n")
             }
 
+        // 게시판 글 리스트
         }else if (cmd.equals("board list")) {
             println("=== 게시판 글 목록 ===")
             for (x in 0 until boardList.size) {
                 println("${boardList[x].idx} / ${boardList[x].subject} / ${boardList[x].regdate} / ${boardList[x].editdate}")
             }
             println()
+        
+        // 존재하지 않는 명령어일 때
         }else {
             println("'${cmd}'는 존재하지 않는 명령어입니다.")
         }
-
-
     }
 
     println("=== 게시판 글 작성 프로그램 종료 ===")
@@ -77,4 +88,4 @@ fun main() {
 }
 
 data class Board (val idx : Int, val subject : String, val contents : String, val regdate : String,
-val editdate : String, )
+val editdate : String)
